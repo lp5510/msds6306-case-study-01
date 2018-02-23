9b35541ad037ec17511258ededd08df0bae559ce
Luke Pierce <luke.pierce@tx.rr.com>
2018-02-22 22:20:48 -0600

    Update 00_LoadAndPrepare.R
    Added code to replace special characters with proper character data or punctuation symbols in the beer and brwy data frames.

--- code/00_LoadAndPrepare.R
+++ code/00_LoadAndPrepare.R
@@ -15,8 +15,51 @@ brewery_url <- paste(data_root_url, "Breweries.csv", sep="");
 beer <- read.csv(url(beer_url), header = TRUE, sep=",", row.names = NULL)
 brewery <- read.csv(url(brewery_url), header = TRUE, sep=",", row.names = NULL)
 
-
 #===============================================================================
+
+# Replace/remove erroneous special characters present in Dataframes for Beer and Brewery
+
+# Beer file
+
+# Special character replacement/removal
+
+# Replace "â€™" with "'"
+beer$Name <- gsub("â€™", "'", beer$Name)
+
+# Replace "Ã¶" with "o"
+beer$Name <- gsub("Ã¶", "o", beer$Name)
+
+# Replace "Â°" with "°"
+beer$Name <- gsub("Â°", "°", beer$Name)
+
+# Replace "Ã©" with "e'"
+beer$Name <- gsub("Ã©", "e'", beer$Name)
+
+# Replace "Ã" with "i'"
+beer$Name <- gsub("Ã", "i'", beer$Name)
+
+# Replace "'¼" with "o'"
+beer$Name <- gsub("'¼", "o", beer$Name)
+
+# Remove "â„¢" character
+beer$Name <- gsub("â„¢", "", beer$Name)
+
+# Replace "Ã¶" with "o"
+beer$Style <- gsub("Ã¶", "o", beer$Style)
+
+# Replace "Ã¤" with "a"
+beer$Style <- gsub("Ã¤", "a", beer$Style)
+
+# Replace "Ã¨" with "e"
+beer$Style <- gsub("Ã¨", "e", beer$Style)
+
+# Brewery file
+
+# Replace "Ã¶" with "o"
+brwy$Name <- gsub("Ã¶", "o", brwy$Name)
+
+# ========================================================================================
+
 # Modify column/variable names on the Dataframes for Beer and Brewery
 
 # in beer df rename column "Name" to "Beer" 
