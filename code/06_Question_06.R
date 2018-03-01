9f5dbdce707451268c025619fb522d7047552705
Luke Pierce <luke.pierce@tx.rr.com>
2018-03-01 13:56:33 -0600

    Fix typo

--- code/06_Question_06.R
+++ code/06_Question_06.R
@@ -1,10 +1,8 @@
 ## @knitr Question_06
 
 #
-
 # Remove NA values only for ABV variable
-brewbeer_ABV <- beerbrew_NA[complete.cases(brewbeer[ , 5]),]
-
+beerbrew_ABV <- beerbrew_NA[complete.cases(beerbrew_NA[ , 5]),]
 #
 # Case Study 01 : Question 06) Summary Statistics for the ABV variable
 summary(beerbrew_ABV$ABV)
