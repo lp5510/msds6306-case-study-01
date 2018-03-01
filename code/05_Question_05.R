3829a272f25db7315672881dae3fc8a2d993899b
Luke Pierce <luke.pierce@tx.rr.com>
2018-02-28 23:10:41 -0600

    Use brewbeer_NA for individual IBU/ABV analysis

--- code/05_Question_05.R
+++ code/05_Question_05.R
@@ -4,7 +4,7 @@
 #
 # Case Study 01 : Question 05) State with highest ABV? Highest IBU?
 # Most alcoholic beer (Kentucky)
-dplyr::top_n(beerbrew, 1, ABV)
+dplyr::top_n(beerbrew_NA, 1, ABV)
 
 # Most bitter beer (Oregon)
-dplyr::top_n(beerbrew, 1, IBU)
+dplyr::top_n(beerbrew_NA, 1, IBU)
