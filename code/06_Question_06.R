e20609d9cad9398ebecd6ef136092ca16172e72d
Luke Pierce <luke.pierce@tx.rr.com>
2018-03-01 13:54:41 -0600

    Remove NA values only for ABV variable

--- code/06_Question_06.R
+++ code/06_Question_06.R
@@ -1,6 +1,10 @@
 ## @knitr Question_06
 
 #
+
+# Remove NA values only for ABV variable
+brewbeer_ABV <- beerbrew_NA[complete.cases(brewbeer[ , 5]),]
+
 #
 # Case Study 01 : Question 06) Summary Statistics for the ABV variable
-summary(beerbrew$ABV)
+summary(beerbrew_ABV$ABV)
