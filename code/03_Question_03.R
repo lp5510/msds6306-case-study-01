e91fe0bd179daefbf76dfa42a873e5205feeb96f
Luke Pierce <luke.pierce@tx.rr.com>
2018-03-02 20:08:24 -0600

    Comment out unneeded code

--- code/03_Question_03.R
+++ code/03_Question_03.R
@@ -3,8 +3,10 @@
 #
 #
 # Case Study 01 : Question 03) Report NA in each column
-q3_out <- capture.output(sapply(beerbrew, function(x) sum(is.na(x))))
-q3_out
+sapply(beerbrew, function(x) sum(is.na(x)))
+
+# q3_out <- capture.output(sapply(beerbrew, function(x) sum(is.na(x))))
+# q3_out
 
 #
 source('code/03_Question_03.tidy.R')
